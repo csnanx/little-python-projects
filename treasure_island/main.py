9d7204b1ed6df72ec0cfa13ee8f995e4ada4bc2f
csnanx <210231094+csnanx@users.noreply.github.com>
2025-05-18 22:16:52 +0530

    Create Treaure Island game

--- treasure_island/main.py
+++ treasure_island/main.py
@@ -1,23 +1,22 @@
-print("Welcome to Python Pizza Deliveries!")
-size = input("What size pizza do you want? S, M or L: ")
-pepperoni = input("Do you want pepperoni on your pizza? Y or N: ")
-extra_cheese = input("Do you want extra cheese? Y or N: ")
-bill = 0
+print("Welcome to Treasure Island.\nYour mission is to find the treasure.")
 
-if size == 'S':
-    bill += 15
-    if pepperoni == 'Y':
-        bill += 2
-elif size == 'M':
-    bill += 20
-    if pepperoni == 'Y':
-        bill += 3
-elif size == 'L':
-    bill += 25
-    if pepperoni == 'Y':
-        bill += 3
+left_right = input("There is a cross road.\nWhich direction to do want to go?\nLeft or Right?\n").lower()
 
-if extra_cheese == 'Y':
-    bill += 1
+if left_right == "right":
+    print("You fell into a hole.\nGame Over.")
+elif left_right == "left":
+    swim_wait = input("There is a huge river in front of you.\nDo you want to swim or wait?\n").lower()
+    
+    if swim_wait == "swim":
+        print("You were attacked by Piranhas.\nYou lose.")
+    elif swim_wait == "wait":
+        print("A boat came to take you to the other side of the river.\nThere is a beautiful castle if front of you.\nYou enter the castle.")
+        
+        door = input("There are three doors before you.\nRed, Blue and a Yellow door.\nWhich door do you choose?\n").lower()
 
-print(f"Your final bill is: ${bill}.")
+        if door == 'red':
+            print("Ahhh! It's hot in here.\nThere is fire everywhere.\nI'm Burning\nGame over.")
+        elif door == 'blue':
+            print("There is water all around.\nOh no.....\nI can't breathe.\nGame over.")
+        elif door == 'yellow':
+            print("It's a room full of gold.\nYou Win!")
