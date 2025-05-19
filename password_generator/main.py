b5747b3023eaea215ff4535411b5cb3cb3484093
csnanx <210231094+csnanx@users.noreply.github.com>
2025-05-19 22:26:21 +0530

    Add logic to create a password list in order

--- password_generator/main.py
+++ password_generator/main.py
@@ -1,3 +1,5 @@
+import random
+
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
@@ -7,3 +9,18 @@ letters_number = int(input("How many letters would you like in your password?\n"
 symbols_number = int(input("How many symbols would you like?\n"))
 numbers_number = int(input("How many numbers would you like?\n"))
 
+password_list = []
+
+for i in range(letters_number):
+    random_letter = random.choice(letters)
+    password_list.append(random_letter)
+
+for i in range(symbols_number):
+    random_symbol = random.choice(symbols)
+    password_list.append(random_symbol)
+
+for i in range(numbers_number):
+    random_number = random.choice(numbers)
+    password_list.append(random_number)
+
+print(f"Your password is: {password_list}")
