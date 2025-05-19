e95a769846d76b82b08e62de49e0a54b105df980
csnanx <210231094+csnanx@users.noreply.github.com>
2025-05-19 22:21:07 +0530

    Add code to take numbers of letters from user

---
+++ password_generator/main.py
@@ -0,0 +1,9 @@
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to password generator!")
+letters_number = int(input("How many letters would you like in your password?\n"))
+symbols_number = int(input("How many symbols would you like?\n"))
+numbers_number = int(input("How many numbers would you like?\n"))
+
