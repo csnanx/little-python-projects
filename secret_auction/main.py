b669284023a70add160a615ca966b6746b765bc8
csnanx <210231094+csnanx@users.noreply.github.com>
2025-05-23 20:44:38 +0530

    Add code to store name and bid to a dictionary

--- secret_auction/main.py
+++ secret_auction/main.py
@@ -1,3 +1,18 @@
 from art import gavel_art
 
 print(gavel_art)
+print("Welcome to The Secret Auction")
+
+auction_data = {}
+other_bidders = True
+
+while other_bidders:
+    name = input("What is your name? ")
+    bid = int(input("What is your bid? $"))
+    auction_data[name] = bid
+
+    more_bidders = input("Are there any other bidders? Type 'yes' or 'no': ")
+    if more_bidders == "no":
+        other_bidders = False
+    elif more_bidders == "yes":
+        print("\n" * 50)
